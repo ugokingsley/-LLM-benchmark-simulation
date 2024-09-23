5576069571f30fa8b77777f2bc13aa9dbcf85f34
ugokingsley <ugokingsley5@gmail.com>
2024-09-23 10:44:06 +0100

    feat(app):Expose metric to graphana

--- app/main.py
+++ app/main.py
@@ -1,12 +1,15 @@
-# app/main.py
-
 from fastapi import FastAPI
 from app.routes import benchmark
+from prometheus_fastapi_instrumentator import Instrumentator
+
 
 app = FastAPI(debug=True)
 
 # Include the benchmark routes
 app.include_router(benchmark.router, prefix="/api")
+
+# Instrument the FastAPI app, This exposes a /metrics endpoint that Prometheus can scrape.
+Instrumentator().instrument(app).expose(app)
 
 
 @app.get("/")
